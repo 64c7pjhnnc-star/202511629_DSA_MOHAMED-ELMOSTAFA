b68fc1e7a95ca1ae52e749948c2a4f1cc5fd914a
64c7pjhnnc-star <64c7pjhnnc@privaterelay.appleid.com>
2026-05-15 14:07:35 +0400

    Add files via upload

---
+++ Adding Numbers (1).cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+float addNumbers(float a, float b, float c, float d) {
+    return a + b + c + d;
+}
+
+int main() {
+
+    cout << addNumbers(10, 20, 30, 40);
+
+    return 0;
+}
